3b9857e95cf443ebe9db2cbb2e24b41aa417e740
Pavel Shapialevich <Pavel.Shapialevich@student.ajp.edu.pl>
2023-10-26 13:48:05 +0200

    beautify delete_item form page

--- delete_item_form.php
+++ delete_item_form.php
@@ -2,13 +2,16 @@
 <html>
 <head>
     <title>Usuń pozycję</title>
+    <link href="static/css/bootstrap.min.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-    <h2>Usuń pozycję</h2>
-    <form action="delete_item.php" method="post">
-        <label for="item_id">Identyfikator pozycji:</label>
-        <input type="text" id="item_id" name="item_id" required><br>
-        <input type="submit" value="Usuń">
+    <form action="delete_item.php" method="post" class="col-lg-6 offset-lg-3 mt-5">
+        <h2>Usuń pozycję</h2>
+        <div class="form-group">
+            <label for="item_id">Identyfikator pozycji:</label>
+            <input type="text" class="form-control" id="item_id" name="item_id" required><br>
+        </div>
+        <button type="submit" class="btn btn-primary">Usuń</button>
     </form>
 </body>
 </html>
