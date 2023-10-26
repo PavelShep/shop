04478924c9105e63e6a4002608465027bc04070e
Pavel Shapialevich <Pavel.Shapialevich@student.ajp.edu.pl>
2023-10-26 13:41:41 +0200

    beautify add_item form page

--- add_item_form.php
+++ add_item_form.php
@@ -1,18 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Dodanie nowego elementu</title>
+    <title>Dodanie nowego towaru</title>
+    <link href="static/css/bootstrap.min.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-    <h2>Dodanie nowego elementu</h2>
-    <form action="add_item.php" method="post" enctype="multipart/form-data">
-        <label for="name">Nazwa:</label>
-        <input type="text" id="name" name="name" required><br>
-        <label for="price">Cena:</label>
-        <input type="text "id="price" name="price"></input><br>
-        <label for="image">Zdjęcie:</label>
-        <input type="file" id="image" name="image" accept="image/*" required><br>
-        <input type="submit" value="Dodać">
+    <form action="add_item.php" method="post" enctype="multipart/form-data" class="col-lg-6 offset-lg-3 mt-5">
+        <h2>Dodawanie nowego towaru</h2>
+        <div class="form-group">
+            <label for="name">Nazwa towara</label>
+            <input type="text" class="form-control" id="name" name="name" placeholder="..." required>
+        </div>
+        <div class="form-group">
+            <label for="price">Cena (zł)</label>
+            <input type="price" class="form-control" id="price" name="price" placeholder="..." required>
+        </div>
+        <div class="form-group">
+            <label for="image">Zdjęcie</label>
+            <input type="file" class="form-control-file" id="image" name="image" accept="image/*" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Dodać</button>
     </form>
 </body>
 </html>
